d506b8db4c8496580e1fddfe3c34960c70212196
luilambu <luislambuley@gmail.com>
2020-12-22 13:22:47 -0500

    post compras

--- src/components/comprasComponent.vue
+++ src/components/comprasComponent.vue
@@ -2,19 +2,20 @@
  <div id="CrearCompranpm">
    <div>
      <h1>Agregar Compra</h1>
-     <form name="form" id="form" v-on:submit.prevent="procesar()">
-        <p> Id_Compra: <input type="numb" name="id_compra" placeholder="Id_Compra" class="form-comtrol" v-model= "compras.id_compra"/></p>
+     <form name="form" id="form" v-on:submit.prevent="guardarCompra()">
+        <p> No_Compra: <input type="numb" name="id_compra" placeholder="Id_Compra" class="form-comtrol" v-model= "compras.id_compra"/></p>
         <p> Cedula: <input tipe="numb" name="cedula" placeholder="cedula" class="form-comtrol" v-model= "compras.cedula"/></p>
-        <p>Nombre_Producto: <input tipe="text" name="Nombre_Producto" placeholder="Nombre_Producto" class="form-comtrol" v-model= "compras.Nombre_producto"/></p>
+        <p>Nombre_Producto: <input tipe="text" name="Nombre_Producto" placeholder="Nombre_Producto" class="form-comtrol" v-model= "compras.nombre_producto"/></p>
         <p>Cantidad: <input tipe="numb" name="Cantidad" placeholder="Cantidad" class="form-comtrol" v-model= "compras.cantidad"/></p>
         <p>Precio: <input tipe="numb" name="Precio" placeholder="Precio" class="form-comtrol" v-model= "compras.Precio"/> </p>
         <p>Total: <input tipe="numb" name="Total" placeholder="Total" class="form-comtrol" v-model= "compras.Total"/></p>
        
         <hr />
-        <button v-on:click="guardarCompra"> Agregar</button>
-        <input type="submit" value="">
+        <!--<button v-on:click="guardarCompra"> Agregar</button>-->
+        <button type="submit">guardar</button>
      </form>
     </div>
+
      <button v-on:click="consultacompras"> consulta compras</button>
      <table class="table">
           <thead>
@@ -32,12 +33,12 @@
             
              <tr>
 
-               <td>{{id_compra}}</td>
-               <td>{{Cedula}}</td>
-               <td>{{nombre_producto}}</td>
-               <td>{{cantidad}}</td>
-               <td>{{precio}}</td>
-               <td>{{Total}}</td>
+               <td>{{compras.id_compra}}</td>
+               <td>{{compras.cedula}}</td>
+               <td>{{compras.nombre_producto}}</td>
+               <td>{{compras.cantidad}}</td>
+               <td>{{compras.Precio}}</td>
+               <td>{{compras.Total}}</td>
              </tr>
         </tbody>    
     </div>
@@ -56,35 +57,42 @@
             cedula:"",
             nombre_producto:"",
             cantidad:"",
-            precio: "",
-            Total:"",
+            precio:"",
+            Total:""
             },
-
         }
       },
     
 
   methods: {
     consultacompras(){
-        axios.get("https://minisap01.herokuapp.com/clientes/compras/"+this.cedula)
+        console.log(this.compras.cedula)
+        axios.get("https://minisap01.herokuapp.com/clientes/compras/"+this.compras.cedula)
         .then(data =>{
-              
                 console.log(data);
-                this.makeToast("Hecho","cliente creado","success");
+
+                alert("Hecho","compras consultadas");
             }).catch( e =>{
                 console.log(e);
-                 this.makeToast("Error","Error al guardar","error");
+                alert("Error al consultar");
             })
     },
     guardarCompra(){
-      
-            axios.post("https://minisap01.herokuapp.com/clientes/compras/"+this.data)
-            .then(data =>{
-                console.log(data);
-                this.makeToast("Hecho","cliente creado","success");
-            }).catch( e =>{
-                console.log(e);
-                 this.makeToast("Error","Error al guardar","error");
+      console.log(this.compras)
+            axios.post("https://minisap01.herokuapp.com/clientes/compras/agregar/",{
+              "id_compra": this.compras.id_compra,
+     	        "cc_cliente": this.compras.cedula,
+              "nombre_producto": this.compras.nombre_producto,
+              "cantidad": this.compras.cantidad,
+              "precio_pub": this.compras.Precio,
+              "costo_total": this.compras.Total 
+            })
+            .then(response =>{
+                console.log(Response);
+                alert("cliente creado","success");
+            }).catch( error =>{
+                console.log(response);
+                alert("Error al guardar","fallido");
             })
     },
   addExpense(e) {
